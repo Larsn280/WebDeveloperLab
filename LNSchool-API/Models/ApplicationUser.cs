df701bcf2d1442699cdb0dc7b4fa1ce400b8e53b
larsn280 <larsn280@hotmail.com>
2023-02-25 17:36:30 +0100

    AddedFirstNameAndLastName

--- LNSchool-API/Models/ApplicationUser.cs
+++ LNSchool-API/Models/ApplicationUser.cs
@@ -4,6 +4,10 @@ namespace LNSchool_API.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName {get; set;}
+
+        public string? LastName {get; set;}
+
         public string? UserType {get; set;}
     }
 }
